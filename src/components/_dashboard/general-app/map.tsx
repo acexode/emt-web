92cd4a4a906b4adc989c01fc243280f1f75c344b
Ahmed Dambatta Hassan <97020889+amirdbt1@users.noreply.github.com>
2023-08-23 18:07:38 +0100

    updated map

--- src/components/_dashboard/general-app/map.tsx
+++ src/components/_dashboard/general-app/map.tsx
@@ -1,5 +1,5 @@
 import { Card } from "@mui/material";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import ambulanceIcon from "../../../assets/ambulance.svg"
 import L from "leaflet";
@@ -14,22 +14,12 @@ interface IMap {
   newAmbulances?:any
 }
  const AmbulanceMap:FC<IMap> = ({newAmbulances}) => {
-    const [ambulances, setAmbulances] = useState<any>(newAmbulances);
-    useEffect(() => {
-        // Simulated real-time data
-        // Replace this with your real data source (WebSocket, API, etc.)
-        const interval = setInterval(() => {
-         
-          setAmbulances(newAmbulances);
-        }, 2000);
-    
-        return () => clearInterval(interval);
-      }, []);
+
   return (
     <Card>
       <MapContainer center={[9.0820, 8.6753]} zoom={7} style={{ height: '400px' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {ambulances.map((ambulance:any) => (
+      {newAmbulances.map((ambulance:any) => (
         <Marker icon={AmbulanceIcon} key={ambulance.id} position={[ambulance.lat, ambulance.lng]}>
           <Popup>{ambulance.name}</Popup>
         </Marker>
